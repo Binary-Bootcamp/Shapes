91ae1950d4b2f1a98864c0c4b8ee34aaf8e7d464
Jonathan Peypops <jonathanpeypops@msn.com>
2015-05-18 13:52:20 +0200

    added a launcher and a shapez interface

---
+++ src/be/vdab/binarybootcamp/Vierkant.java
@@ -0,0 +1,11 @@
+package be.vdab.binarybootcamp;
+
+public class Vierkant implements Shapez {
+	public double zijde;
+	
+	public String toString() {
+		
+		return "een vierkant met zijde " + zijde;
+	}
+
+}
